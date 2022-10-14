2b9dc0519e06dd9dfee21f66ad9942df3fe639da
HexaField <joshfield999@gmail.com>
2022-10-14 12:07:01 +1100

    update service

--- server/services/services.ts
+++ server/services/services.ts
@@ -3,6 +3,7 @@ import { Engine } from "@xrengine/engine/src/ecs/classes/Engine"
 import { initSystems } from "@xrengine/engine/src/ecs/functions/SystemFunctions"
 import { SystemUpdateType } from "@xrengine/engine/src/ecs/functions/SystemUpdateType"
 import { Application, ServerMode } from "@xrengine/server-core/declarations"
+import config from "@xrengine/server-core/src/appconfig"
 
 declare module '@xrengine/common/declarations' {
   interface ServiceTypes {
@@ -23,7 +24,7 @@ export const jarvisRequest = (app: Application) => {
 }
 
 async function JARVIS (app: Application) {
-  if (app.serverMode !== ServerMode.Analytics) return
+  if (app.serverMode !== ServerMode.Analytics || config.db.forceRefresh) return
 
   app.use('jarvis-request', {
     get: jarvisRequest(app)
